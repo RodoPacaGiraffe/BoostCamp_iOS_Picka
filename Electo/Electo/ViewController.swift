b1324dd35f93e7e709b2d3d50d52593ba28a1549
byung-soo kwon <planon21@gmail.com>
2017-08-07 20:42:19 +0900

    Initial Commit

---
+++ Electo/Electo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Electo
+//
+//  Created by byung-soo kwon on 2017. 8. 7..
+//  Copyright © 2017년 RodoPacaGiraffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
