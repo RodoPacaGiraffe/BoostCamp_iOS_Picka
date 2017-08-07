07c960cdb0d6e9c467a564068984b95acf085016
byung-soo kwon <planon21@gmail.com>
2017-08-08 00:34:07 +0900

    Real Initial Migration

--- Electo/Electo/AppDelegate.swift
+++ Electo/Electo/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Electo
 //
-//  Created by byung-soo kwon on 2017. 8. 8..
+//  Created by RodoPacaGiraffe on 2017. 8. 4..
 //  Copyright © 2017년 RodoPacaGiraffe. All rights reserved.
 //
 
