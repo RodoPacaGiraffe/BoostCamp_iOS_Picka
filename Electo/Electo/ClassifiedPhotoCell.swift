12dec5fec17a5bbcd2b720da7b4283bdefb8faa0
wnstkdyu <wnstkdyu@gmail.com>
2017-08-08 21:26:41 +0900

    이미지가 4개 이상일 시 표시 label 설정

--- Electo/Electo/ClassifiedPhotoCell.swift
+++ Electo/Electo/ClassifiedPhotoCell.swift
@@ -30,7 +30,7 @@ class ClassifiedPhotoCell: UITableViewCell {
             imageStackView.addArrangedSubview(imageView)
         }
         
-        if photoImages.count < 4 {
+        if photoImages.count <= 4 {
             for index in photoImages.indices {
                 guard let subImageView = imageStackView.arrangedSubviews[index] as? UIImageView else { break }
                 subImageView.image = photoImages[index]
@@ -40,7 +40,35 @@ class ClassifiedPhotoCell: UITableViewCell {
                 guard let subImageView = imageStackView.arrangedSubviews[index] as? UIImageView else { break }
                 subImageView.image = photoImages[index]
             }
+            setLabel()
         }
+    }
+    
+    func setLabel() {
+        guard let lastSubImageView = imageStackView.arrangedSubviews.last as? UIImageView else { return }
+        lastSubImageView.image = lastSubImageView.image?.alpha(0.5)
+        let moreImagesLabel: UILabel = .init()
+        lastSubImageView.addSubview(moreImagesLabel)
+        
+        let numberOfMoreImages = cellImages.count - 4
+        moreImagesLabel.numberOfLines = 2
+        let moreAttributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 25)]
+        let moreImagesText = NSMutableAttributedString(string: "+\(numberOfMoreImages)\n장의 사진", attributes: moreAttributes)
+        
+        if numberOfMoreImages >= 10 {
+            moreImagesText.addAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 15)],
+                                         range: NSRange(location: 3, length: 6))
+        } else {
+            moreImagesText.addAttributes([NSFontAttributeName: UIFont.systemFont(ofSize: 15)],
+                                         range: NSRange(location: 3, length: 5))
+        }
+        moreImagesLabel.attributedText = moreImagesText
+        moreImagesLabel.textAlignment = .center
+        moreImagesLabel.textColor = UIColor.black
+
+        moreImagesLabel.translatesAutoresizingMaskIntoConstraints = false
+        moreImagesLabel.centerXAnchor.constraint(equalTo: lastSubImageView.centerXAnchor).isActive = true
+        moreImagesLabel.centerYAnchor.constraint(equalTo: lastSubImageView.centerYAnchor).isActive = true
     }
     
     func clearStackView() {
