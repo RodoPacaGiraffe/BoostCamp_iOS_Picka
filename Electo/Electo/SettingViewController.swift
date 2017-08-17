996b01e997e60f3be8c7b705fdce112e2423fccf
wnstkdyu <wnstkdyu@gmail.com>
2017-08-17 15:45:37 +0900

    UISlider 구현 - 이미지뷰의 스택뷰로 구간별 tick 집어넣음

--- Electo/Electo/SettingViewController.swift
+++ Electo/Electo/SettingViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class SettingViewController: UITableViewController {
     
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var tickStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setTickStackView()
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        switch sender.value {
+        case 1..<1.5:
+            sender.setValue(1.0, animated: true)
+        case 1.5..<2.5:
+            sender.setValue(2.0, animated: true)
+        case 2.5..<3.5:
+            sender.setValue(3.0, animated: true)
+        case 3.5..<4.5:
+            sender.setValue(4.0, animated: true)
+        default:
+            sender.setValue(5.0, animated: true)
+        }
+    }
+    
     @IBAction func networkAllowSwitch(_ sender: UISwitch) {
         print(sender.state)
         if sender.isOn {
@@ -28,5 +52,11 @@ class SettingViewController: UITableViewController {
             print("off")
             Constants.dataAllowed = false
         }
+    }
+    
+    func setTickStackView() {
+        let sliderWidth: CGFloat = slider.frame.width
+        guard let tickWidth: CGFloat = tickStackView.subviews.first?.frame.width else { return }
+        tickStackView.spacing = (sliderWidth - tickWidth * 5) / 4 - 1
     }
 }
