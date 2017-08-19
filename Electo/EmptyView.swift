fe392c52e1304d3b041f3c619a5d445809ffbc66
wnstkdyu <wnstkdyu@gmail.com>
2017-08-19 16:12:37 +0900

    포토 라이브러리 접근이 거부되었을 때 뷰 구현

---
+++ Electo/EmptyView.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyView.swift
+//  Electo
+//
+//  Created by Alpaca on 2017. 8. 19..
+//  Copyright © 2017년 RodoPacaGiraffe. All rights reserved.
+//
+
+import UIKit
+
+class EmptyView: UIView {
+    
+    class func instanceFromNib() -> EmptyView {
+        guard let emptyView = UINib(nibName: "EmptyView", bundle: nil)
+            .instantiate(withOwner: nil, options: nil).first as? EmptyView else {
+            return EmptyView()
+        }
+        
+        return emptyView
+    }
+}
