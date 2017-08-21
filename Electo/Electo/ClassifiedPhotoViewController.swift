8c71304bcd3f0e3780d350ff27e05a2545f003e5
wnstkdyu <wnstkdyu@gmail.com>
2017-08-21 21:40:02 +0900

    아랍 메인 뷰일 때 인덱스 반대로 해서 수정.

--- Electo/Electo/ClassifiedPhotoViewController.swift
+++ Electo/Electo/ClassifiedPhotoViewController.swift
@@ -291,13 +291,29 @@ class ClassifiedPhotoViewController: UIViewController {
     func showSelectedPhoto(at indexPath: IndexPath) {
         
         guard let detailViewController = storyboard?.instantiateViewController(withIdentifier:  "detailViewController") as? DetailPhotoViewController else { return }
-        let selectedPhotoIndex = getIndexOfSelectedPhoto(from: touchLocation)
+        var selectedPhotoIndex = getIndexOfSelectedPhoto(from: touchLocation)
         let selectedCell = tableView.cellForRow(at: indexPath) as? ClassifiedPhotoCell ?? ClassifiedPhotoCell.init()
+        
+        if Locale.current.languageCode == "ar" {
+            selectedPhotoIndex = Constants.maximumImageView - selectedPhotoIndex - 1
+            
+            guard selectedCell.imageViews[selectedPhotoIndex].image != nil else {
+                dataSetOfTransfer(to: detailViewController, selectedCell: selectedCell, of: indexPath, 0)
+                show(detailViewController, sender: self)
+                return
+            }
+            dataSetOfTransfer(to: detailViewController, selectedCell: selectedCell, of: indexPath,
+                              selectedPhotoIndex)
+            show(detailViewController, sender: self)
+            return
+        }
+        
         guard selectedCell.imageViews[selectedPhotoIndex].image != nil else {
             dataSetOfTransfer(to: detailViewController, selectedCell: selectedCell, of: indexPath, 0)
             show(detailViewController, sender: self)
             return
         }
+        
         
         dataSetOfTransfer(to: detailViewController, selectedCell: selectedCell, of: indexPath, selectedPhotoIndex)
         show(detailViewController, sender: self)
